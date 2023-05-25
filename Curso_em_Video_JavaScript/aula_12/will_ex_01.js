8fa2c12cffdc4f04a6c3d801288ca5e0a0da8038
William Cardoso <williamcardoso74@gmail.com>
2023-05-24 23:40:09 -0300

    Exercício M2S03

--- Curso_em_Video_JavaScript/aula_12/will_ex_01.js
+++
@@ -1,7 +0,0 @@
-// operador ternário
-
-var agora = new Date()
-var nascimento = new Date(1920,01,27)
-var idade = agora.getFullYear() - nascimento.getFullYear()
-var res = (idade < 18) ? 'menor de idade': 'maior de idade'
-console.log(`Você tem ${idade} anos e é ${res}`)
--- Curso_em_Video_JavaScript/aula_14/ambiente.js
+++
@@ -1,8 +0,0 @@
-console.log('Vai começar!')
-
-// com FOR
-for (var c=1; c <= 5; c++) {
-    console.log(c)
-}
-
-console.log('FIM')
