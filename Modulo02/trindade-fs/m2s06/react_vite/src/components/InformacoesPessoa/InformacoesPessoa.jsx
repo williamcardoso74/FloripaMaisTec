2f6641b7aa6dd6a65b923a72886a3e4f4399d72f
William Cardoso <williamcardoso74@gmail.com>
2023-06-09 09:52:37 -0300

    Remoção de arquivos 09 06 2023

--- Modulo02/trindade-fs/m2s06/react_vite/src/components/InformacoesPessoa/InformacoesPessoa.jsx
+++ Modulo02/trindade-fs/m2s06/react_vite/src/components/InformacoesPessoa/InformacoesPessoa.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import './InformacoesPessoa.css';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const InformacoesPessoa = (props) => {
   return (
-    <div className="container alert alert-success" role="alert">
-      <h1>
-        Olá, Meu nome é <strong className="fw-bold text-center">{props.nome}</strong>, tenho {props.idade} anos e gosto de{" "}
-        {props.hobbie}.
-      </h1>
-    </div>
-  );
+  <>
+    <h1>
+      Olá, Meu nome é {props.nome}, tenho {props.idade} anos e gosto de {props.hobbie}.
+    </h1>
+  </>
+  )
 }
 
 InformacoesPessoa.propTypes = {
-  nome: PropTypes.string.isRequired,
-  idade: PropTypes.number.isRequired,
-  hobbie: PropTypes.string,
-};
+    nome: PropTypes.string.isRequired,
+    idade: PropTypes.number.isRequired,
+    hobbie: PropTypes.string,
+}
 
 export default InformacoesPessoa;
+
+
