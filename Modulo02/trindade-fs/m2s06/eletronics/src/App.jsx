9131900942ffb9d148b20215f790484a35c814ce
William Cardoso <williamcardoso74@gmail.com>
2023-06-09 22:35:43 -0300

    Aula React 09 06 2023

---
+++ Modulo02/trindade-fs/m2s06/eletronics/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+/*import CardProduct from "./components/CardProduct";
+import Footer from "./components/Footer"; */
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      {/*<CardProduct/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ Modulo02/trindade-fs/m2s06/eletronics/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Banner.css";
+
+function Banner() {
+  return (
+    <div className="container-fluid banner">
+      <span>ESCOLHA O MELHOR PARA VOCÊ</span>
+      <span>nossos produtos</span>
+    </div>
+  );
+};
+
+export default Banner;
